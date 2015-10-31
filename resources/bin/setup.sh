16570da221089bfe2a398d5d4395ad0d55fa6bff
Martijn van Maurik <martijn@vmaurik.nl>
2015-10-31 12:28:20 +0100

    When selecting another version auto install

--- resources/bin/setup.sh
+++ resources/bin/setup.sh
@@ -141,6 +141,7 @@ install_rvm() {
 
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 > /dev/null || return 1
     curl -sSL https://get.rvm.io | bash -s stable --ruby --with-default-gems='bundler rails'  || return 1
+    echo 'rvm_install_on_use_flag=1' >> ${APP_HOME}/.rvmrc
 
     return 0
 }
